13c1d558e585cb310eda88c65ea79208493e45de
vtdrfuka <80080115+vtdrfuka@users.noreply.github.com>
2022-08-24 12:44:04 -0600

    Adding keepalive=15

--- LoggerNet2MQTTJSON.py
+++ LoggerNet2MQTTJSON.py
@@ -25,7 +25,7 @@ def connect_mqtt():
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
-    client.connect(broker, port)
+    client.connect(broker, port, keepalive=15)
     return client
 
 def publish(client,mqttmsg):
